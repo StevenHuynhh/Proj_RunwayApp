74b7b64fb24228a4006e343a100cfae240cfa4db
Steven Huynh <144557088+StevenHuynhh@users.noreply.github.com>
2024-07-08 16:12:40 -0400

    Pre API setup
    Updated just incase anything breaks

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:hello_world/dbHelper/mongodb.dart';
 import 'package:hello_world/models/shop.dart';
 import 'package:provider/provider.dart';
 import 'pages/Intro_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  MongoDatabase.connect();
   runApp(
     ChangeNotifierProvider(
       create: (context) => Shop(),
