a996fa40ba51f88ca05dc71124dce9993cb5f39f
Rick Leinecker <nmistry0301@gmail.com>
2024-07-19 16:51:35 -0400

    added a very basic non functional search api

--- lib/util/api_endpoints.dart
+++ lib/util/api_endpoints.dart
@@ -1,6 +1,8 @@
+import 'package:http/http.dart' as http;
+
 class ApiEndpoints {
   static const baseUrl =
-      'https://cop4331-runway-0afcfa4da5a9.herokuapp.com/api';
+      'https://projectrunway.tech/api';
 
   static _AuthEndPoints authEndPoints = _AuthEndPoints(); // <- Change this line
 }
@@ -8,4 +10,6 @@ class ApiEndpoints {
 class _AuthEndPoints {
   final String loginEmail = '/signin';
   final String registerEmail = '/signup';
+  final String search = '/search';
 }
+
