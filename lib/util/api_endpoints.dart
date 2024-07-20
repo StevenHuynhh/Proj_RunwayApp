84d58ceff9855e371da31f94349c2bbc9ff5071b
Steven Huynh <144557088+StevenHuynhh@users.noreply.github.com>
2024-07-19 20:28:20 -0400

    Update api_endpoints.dart
    asdasd

--- lib/util/api_endpoints.dart
+++ lib/util/api_endpoints.dart
@@ -1,6 +1,7 @@
+import 'package:http/http.dart' as http;
+
 class ApiEndpoints {
-  static const baseUrl =
-      'https://cop4331-runway-0afcfa4da5a9.herokuapp.com/api';
+  static const baseUrl = 'https://projectrunway.tech/api';
 
   static _AuthEndPoints authEndPoints = _AuthEndPoints(); // <- Change this line
 }
@@ -8,4 +9,5 @@ class ApiEndpoints {
 class _AuthEndPoints {
   final String loginEmail = '/signin';
   final String registerEmail = '/signup';
+  final String search = '/search';
 }
